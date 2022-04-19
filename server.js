ddf627efb68274c737d1cc4030d243f36ad80673
Ermiyas  Hailemichael <yemeerma11@gmail.com>
2022-04-19 12:45:50 -0700

    port listening

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const app = express()
+
 require("dotenv").config()
-
-
 const PORT = process.env.PORT || 4000
+
+app.get("/",(req,res) => {
+    res.send("You are home!")
+})
+
+
+
 
 
 
 app.listen(PORT,()=>{
-    console.log(`Listening...${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 })
